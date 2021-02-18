f90cba8c590569c02310e520eace9f661f5eb7ae
Irma Ortiz <73261730+IrmIris33@users.noreply.github.com>
2021-02-18 11:51:26 -0500

    Delete src/Components directory

--- src/Components/Notification/Notification.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Notification() {
-    return (
-        <div className="notification">
-                <p>COVID-19 TEST REQUIREMENTS FOR ENTRY - LEARN MORE </p>
-        </div>
-    )
-}
