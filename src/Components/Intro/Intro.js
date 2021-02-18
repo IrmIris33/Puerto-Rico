2ee0c24895c95a4f0ac2bff774502586098ad341
IrmIris33 <irmirisortizortiz@outlook.com>
2021-02-18 11:10:57 -0500

    modified Puerto Rico

---
+++ src/Components/Intro/Intro.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Intro() {
+    
+    return (
+        <div>
+            <div className="notification">
+                <p>COVID-19 TEST REQUIREMENTS FOR ENTRY - LEARN MORE </p>
+            </div>
+
+            <div className="intro-videos">
+            
+                <iframe width="1230" height="500" src="https://www.youtube.com/embed/b-FdyuIVDm4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div> 
+
+            <div className="header-title">
+                <h1> The Heart & Soul of the Caribbean</h1>
+                <div className="intro-content">
+                    <p>Puerto Rico is a place where the Old World is mashed up with the new on an island that is small in size but offers an immense bounty of one-of-a-kind experiences. </p>
+
+                    <p>Rich history and culture, exceptional food, pristine beaches, majestic mountains, relaxation, adventure — all packed into one sun-kissed Caribbean paradise. La Isla del Encanto is full of people who are buena gente (which is boricua slang for kind and friendly) and welcoming. Here, you’ll feel like you are family instead of just a visitor. Around every corner, you’ll find a celebration of life, a vibrant cultural experience, food for the soul, and a captivating rhythm.</p>
+
+                    <p>Dare to discover the many treasures of Puerto Rico. Get inspired and start planning your trip. Let us be the first to say, bienvenidos.</p>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+
+
+
+
